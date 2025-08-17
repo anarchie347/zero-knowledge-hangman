dc958a3a1fa0392a93a9bc4f76713afd971b0842
anarchie <116115028+anarchie347@users.noreply.github.com>
2025-08-18 00:02:38 +0100

    Added basic hangman game

--- src/game.py
+++ src/game.py
@@ -1,5 +1,47 @@
-
+import hashlib
 HASHES_FILE_PATH = "hashes"
+
+def printGuessed(guessed):
+    print(" ".join(guessed))
+
+def isWon(guessed):
+    return not any(map(lambda c : c == "_", guessed))
+
+def makeGuess(hashes, guessed, letter):
+    correct = False
+    for i,[h,s] in enumerate(hashes):
+        generatedHash = hashlib.sha512(letter.encode() + s.encode()).hexdigest()
+        if generatedHash == h:
+            guessed[i] = letter
+            correct = True
+    return correct
+
+
+def play(hashes):
+    remainingGuesses = 8
+    wordLen = len(hashes)
+    guessed = ["_"] * wordLen
+    print("Start guessing letters")
+    print(f"You have {remainingGuesses} wrong guesses left")
+    while (not isWon(guessed)) and remainingGuesses > 0:
+        printGuessed(guessed)
+        guesses = input()
+        for g in guesses:
+            correct = makeGuess(hashes,guessed,g)
+            if correct:
+                print(f"'{g}' was correct!")
+            else:
+                remainingGuesses -= 1
+                print(f"'{g}' was wrong!")
+                print(f"You have {remainingGuesses} wrong guesses left")
+    if remainingGuesses > 0:
+        print("Correct!")
+        print(f"You still had {remainingGuesses} wrong guesses left")
+        print("The word was " + "".join(guessed))
+    else:
+        print("Failed!")
+        print("I cant tell you the answer because I don't know either")
+
 
 # returns a 2d array, first index represents letter index, which each contains [salt, hash]
 def parseLine(line):
@@ -29,7 +71,7 @@ def main():
     print("Welcome to Zero Knowledge Hangman")
     num = getWordNumber()
     hashes = getWordHashesFromNumber(num)
-    print(hashes)
+    play(hashes)
 
 
 if __name__ == "__main__":
