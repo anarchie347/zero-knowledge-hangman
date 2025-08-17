0abef2693c350e07f6f82875c2210c7e55736e03
anarchie <116115028+anarchie347@users.noreply.github.com>
2025-08-17 22:35:39 +0100

    added hash reading logic

--- src/game.py
+++ src/game.py
@@ -1,23 +1,35 @@
 
+HASHES_FILE_PATH = "hashes"
 
-
-        
-def getWordHashesFromNumber():
-    pass
+# returns a 2d array, first index represents letter index, which each contains [salt, hash]
+def parseLine(line):
+    components = line.split("|")
+    return [c.split(",") for c in components]
+    
+# gets parsed hashes for a given word number
+def getWordHashesFromNumber(num):
+    with open(HASHES_FILE_PATH) as file:
+        for i,line in enumerate(file):
+            if (i == 0):
+                lineCount = int(line)
+                num = num % lineCount
+            if i == num:
+                return parseLine(line)
 
 def getWordNumber():
     print("Enter a number or leave blank to choose a random one:")
     numStr = input()
-    while not numStr.isdecimal():
-        print("Enter a non-negative integer")
+    while (not numStr.isdecimal()) or int(numStr) == 0:
+        print("Enter a positive integer")
         numStr = input()
     num = int(numStr)
     return num
 
 def main():
     print("Welcome to Zero Knowledge Hangman")
-    print(getWordNumber())
-
+    num = getWordNumber()
+    hashes = getWordHashesFromNumber(num)
+    print(hashes)
 
 
 if __name__ == "__main__":
