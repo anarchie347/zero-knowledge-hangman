2175b4f05a0a68b9f18e2884488b0ec8f78c4b77
anarchie <116115028+anarchie347@users.noreply.github.com>
2025-08-17 23:25:05 +0100

    added backend hasher function

---
+++ src/hasher.py
@@ -0,0 +1,39 @@
+import secrets, hashlib
+
+WORDS_PATH = "words"
+HASHES_PATH = "hashes"
+SALT_LEN = 64
+
+
+def saltAndHash(val):
+    encoded = val.lower().encode()
+    salt = secrets.token_urlsafe(SALT_LEN)
+    hexdigest = hashlib.sha512(encoded + salt.encode()).hexdigest()
+    return f"{hexdigest},{salt}"
+
+def hashWord(word):
+    result = saltAndHash(word) + "|"
+    result += "|".join([ saltAndHash(letter) for letter in word])
+    return result
+    
+def countWordsLen():
+    with open(WORDS_PATH, "r") as file:
+       for i,_ in enumerate(file):
+           pass
+    return i + 1
+
+
+
+def main():
+    with open(WORDS_PATH, "r") as wordList:
+        wordCount = countWordsLen()
+        with open(HASHES_PATH, "w") as hashList:
+            hashList.write(f"{wordCount},{SALT_LEN}\n")
+            for line in wordList:
+                hashList.write(hashWord(line.strip()) + "\n")
+
+
+
+
+if __name__ == "__main__":
+    main()
